abdab0bbcfbc8c0d88f8066a3c596bcaf6d5d21c
vinchan <chenwenfeng@kkgroup.cn>
2025-05-15 18:13:18 +0800

    feat(*):add NaturalLanguageProcessing Enum

--- backend/magic-service/app/Domain/ModelAdmin/Constant/NaturalLanguageProcessing.php
+++ backend/magic-service/app/Domain/ModelAdmin/Constant/NaturalLanguageProcessing.php
@@ -1,10 +1,17 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * Copyright (c) The Magic , Distributed under the software license
+ */
+
 namespace App\Domain\ModelAdmin\Constant;
 
-class NaturalLanguageProcessing
+enum NaturalLanguageProcessing
 {
-    const string DEFAULT = 'default';
-    const string EMBEDDING = 'embedding'; // 嵌入
-    const string LLM = 'llm'; // 大语言
+    public const string DEFAULT = 'default';
+
+    public const string EMBEDDING = 'embedding'; // 嵌入
+
+    public const string LLM = 'llm'; // 大语言
 }
