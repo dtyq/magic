e0294f1c1d5783f9976bbfa267aeea30842ad36e
Vinchan <ademo@vip.qq.com>
2025-05-16 23:29:00 +0800

    feat(*):resolve conversation

--- backend/magic-service/app/Domain/ModelAdmin/Constant/NaturalLanguageProcessing.php
+++ backend/magic-service/app/Domain/ModelAdmin/Constant/NaturalLanguageProcessing.php
@@ -7,11 +7,11 @@ declare(strict_types=1);
 
 namespace App\Domain\ModelAdmin\Constant;
 
-enum NaturalLanguageProcessing
+enum NaturalLanguageProcessing: string
 {
-    public const string DEFAULT = 'default';
+    case DEFAULT = 'default';
 
-    public const string EMBEDDING = 'embedding'; // 嵌入
+    case EMBEDDING = 'embedding'; // 嵌入
 
-    public const string LLM = 'llm'; // 大语言
+    case LLM = 'llm'; // 大语言
 }
