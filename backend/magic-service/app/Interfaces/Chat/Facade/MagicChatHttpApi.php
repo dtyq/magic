db999645322f7837ee02a86f6d71eef37bd834da
liangchen <liangpeng@kkgroup.cn>
2025-06-25 16:18:54 +0800

    composer u

--- backend/magic-service/app/Interfaces/Chat/Facade/MagicChatHttpApi.php
+++ backend/magic-service/app/Interfaces/Chat/Facade/MagicChatHttpApi.php
@@ -458,7 +458,6 @@ class MagicChatHttpApi extends AbstractApi
             $historyMessages = $this->magicChatMessageAppService->getConversationChatCompletionsHistory(
                 $authorization,
                 $conversationId,
-                '',
                 20,
                 $topicId
             );
