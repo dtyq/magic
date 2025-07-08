8f736eb87c2d7eb31ea7ecf48f8a10a09284cd91
Rock <383329495@qq.com>
2025-07-09 00:12:16 +0800

    feat(SuperAgent): enhance task handling by introducing TaskMessageEntity and refactoring task initialization logic

--- backend/super-magic-module/src/Application/SuperAgent/Service/HandleAgentMessageAppService.php
+++ backend/super-magic-module/src/Application/SuperAgent/Service/HandleAgentMessageAppService.php
@@ -14,6 +14,7 @@ use Dtyq\SuperMagic\Application\SuperAgent\DTO\TaskMessageDTO;
 use Dtyq\SuperMagic\Domain\SuperAgent\Constant\TaskFileType;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\TaskEntity;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\TopicEntity;
+use Dtyq\SuperMagic\Domain\SuperAgent\Entity\TaskMessageEntity;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\ValueObject\ChatInstruction;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\ValueObject\MessageType;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\ValueObject\TaskContext;
@@ -54,6 +55,7 @@ class HandleAgentMessageAppService extends AbstractAppService
     /**
      * Handle Agent Message - Main Entry Point
      * Responsible for overall business process orchestration.
+     * agent send message to user
      */
     public function handleAgentMessage(TopicTaskMessageDTO $messageDTO): void
     {
@@ -348,7 +350,8 @@ class HandleAgentMessageAppService extends AbstractAppService
             messageId: $messageData['messageId']
         );
 
-        $this->taskDomainService->recordTaskMessage($taskMessageDTO);
+        $taskMessageEntity = TaskMessageEntity::taskMessageDTOToTaskMessageEntity($taskMessageDTO);
+        $this->taskDomainService->recordTaskMessage($taskMessageEntity);
     }
 
     /**
