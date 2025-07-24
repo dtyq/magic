ca1dd324a465db1ee36eb0b8496e0b15bbc891a0
Rock <383329495@qq.com>
2025-07-24 16:54:25 +0800

    refactor(super-agent): update API routes and method signatures for improved clarity

--- backend/super-magic-module/src/Interfaces/SuperAgent/Facade/SandboxApi.php
+++ backend/super-magic-module/src/Interfaces/SuperAgent/Facade/SandboxApi.php
@@ -82,6 +82,8 @@ class SandboxApi extends AbstractApi
 
         // 从请求中创建DTO
         $apiKey = $this->getApiKey();
+
+        var_dump($apiKey, 'apiKey======');
         if (empty($apiKey)) {
             ExceptionBuilder::throw(GenericErrorCode::ParameterMissing, 'The api key of header is required');
         }
