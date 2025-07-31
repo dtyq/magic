5cc4ec95046b3bd0280f5436b45b8f9eac75c185
coderLee <lizengquan@kkgroup.cn>
2025-07-31 20:28:39 +0800

    feat(knowledge-base): Optimize the structure of knowledge synchronization information

--- backend/magic-service/app/Application/KnowledgeBase/Event/Subscribe/KnowledgeBaseFragmentSyncSubscriber.php
+++ backend/magic-service/app/Application/KnowledgeBase/Event/Subscribe/KnowledgeBaseFragmentSyncSubscriber.php
@@ -66,7 +66,13 @@ readonly class KnowledgeBaseFragmentSyncSubscriber implements ListenerInterface
                         'user_id' => $fragment->getModifier(),
                         'business_id' => $knowledge->getCode(),
                         'source_id' => 'fragment_saved',
-                        'knowledge_name' => $knowledge->getName(),
+                        'knowledge_info' => [
+                            'id' => $knowledge->getId(),
+                            'organization_code' => $knowledge->getOrganizationCode(),
+                            'code' => $knowledge->getCode(),
+                            'name' => $knowledge->getName(),
+                            'business_id' => $knowledge->getBusinessId(),
+                        ],
                     ],
                 ]);
                 $fragment->setVector(Json::encode($embeddings));
