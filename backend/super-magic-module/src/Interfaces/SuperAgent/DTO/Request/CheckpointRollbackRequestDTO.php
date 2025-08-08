bb516425df7a40b0f75736929e0798e77a879ddf
codinghuang <codinghuang@qq.com>
2025-08-08 14:06:05 +0800

    feat(SuperAgent): 添加checkpoint回滚功能，包括请求和响应DTO的实现

---
+++ backend/super-magic-module/src/Interfaces/SuperAgent/DTO/Request/CheckpointRollbackRequestDTO.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Copyright (c) The Magic , Distributed under the software license
+ */
+
+namespace Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Request;
+
+use App\Infrastructure\Core\AbstractRequestDTO;
+
+class CheckpointRollbackRequestDTO extends AbstractRequestDTO
+{
+    protected string $topicId = '';
+    protected string $targetMessageId = '';
+
+    /**
+     * 验证规则.
+     */
+    public static function getHyperfValidationRules(): array
+    {
+        return [
+            'topic_id' => 'required|string',
+            'target_message_id' => 'required|string',
+        ];
+    }
+
+    public static function getHyperfValidationMessage(): array
+    {
+        return [
+            'topic_id.required' => '话题ID不能为空',
+            'target_message_id.required' => '目标消息ID不能为空',
+        ];
+    }
+
+    /**
+     * 属性名称.
+     */
+    public function attributes(): array
+    {
+        return [
+            'topic_id' => '话题ID',
+            'target_message_id' => '目标消息ID',
+        ];
+    }
+
+    public function getTopicId(): string
+    {
+        return $this->topicId;
+    }
+
+    public function setTopicId(string $topicId): void
+    {
+        $this->topicId = $topicId;
+    }
+
+    public function getTargetMessageId(): string
+    {
+        return $this->targetMessageId;
+    }
+
+    public function setTargetMessageId(string $targetMessageId): void
+    {
+        $this->targetMessageId = $targetMessageId;
+    }
+}
