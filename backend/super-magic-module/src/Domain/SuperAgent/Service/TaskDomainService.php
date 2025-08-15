ea71f646cc33cb3d23d673b590ca51ddec10684b
liaobw <liaobingwei@kkgroup.cn>
2025-08-16 00:34:34 +0800

    feat(TaskDomainService, TaskFileDomainService): add content type handling for CSS files in cloud file operations

--- backend/super-magic-module/src/Domain/SuperAgent/Service/TaskDomainService.php
+++ backend/super-magic-module/src/Domain/SuperAgent/Service/TaskDomainService.php
@@ -8,8 +8,10 @@ declare(strict_types=1);
 namespace Dtyq\SuperMagic\Domain\SuperAgent\Service;
 
 use App\Domain\Contact\Entity\ValueObject\DataIsolation;
+use App\Domain\File\Repository\Persistence\CloudFileRepository;
 use App\ErrorCode\GenericErrorCode;
 use App\Infrastructure\Core\Exception\ExceptionBuilder;
+use App\Infrastructure\Core\ValueObject\StorageBucketType;
 use App\Infrastructure\Util\IdGenerator\IdGenerator;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\ScriptTaskEntity;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\TaskEntity;
@@ -33,8 +35,10 @@ use Dtyq\SuperMagic\Infrastructure\ExternalAPI\Sandbox\Volcengine\SandboxService
 use Dtyq\SuperMagic\Infrastructure\ExternalAPI\Sandbox\WebSocket\WebSocketSession;
 use Dtyq\SuperMagic\Infrastructure\ExternalAPI\SandboxOS\Agent\Request\ScriptTaskRequest;
 use Dtyq\SuperMagic\Infrastructure\ExternalAPI\SandboxOS\Agent\SandboxAgentInterface;
+use Dtyq\SuperMagic\Infrastructure\Utils\ContentTypeUtil;
 use Hyperf\Contract\StdoutLoggerInterface;
 use RuntimeException;
+use Throwable;
 
 class TaskDomainService
 {
@@ -44,6 +48,7 @@ class TaskDomainService
         protected TaskRepositoryInterface $taskRepository,
         protected TaskMessageRepositoryInterface $messageRepository,
         protected TaskFileRepositoryInterface $taskFileRepository,
+        protected CloudFileRepository $cloudFileRepository,
         protected StdoutLoggerInterface $logger,
         protected SandboxService $sandboxService,
         protected SandboxAgentInterface $sandboxAgent,
@@ -348,6 +353,20 @@ class TaskDomainService
         // Set parent_id if provided
         if ($parentId !== null) {
             $taskFileEntity->setParentId($parentId);
+        }
+
+        // Css file need to set content type
+        if (! $taskFileEntity->getIsDirectory() && ContentTypeUtil::isSetContentType($taskFileEntity->getFileName())) {
+            try {
+                $this->cloudFileRepository->setHeadObjectByCredential(
+                    $dataIsolation->getCurrentOrganizationCode(),
+                    $fileKey,
+                    ['content_type' => ContentTypeUtil::getContentType($taskFileEntity->getFileName())],
+                    StorageBucketType::SandBox
+                );
+            } catch (Throwable $e) {
+                $this->logger->error('setHeadObjectByCredential error', ['file_name' => $taskFileEntity->getFileName(), 'msg' => $e->getMessage()]);
+            }
         }
 
         // Use insertOrIgnore method, if there's already a record with the same file_key and topic_id, return the existing entity
