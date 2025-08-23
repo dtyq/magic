bdacd76f10e7d577033e80a5b454134cfdfff2b3
lihq1403 <lihqing1403@gmail.com>
2025-08-23 19:04:42 +0800

    feat(TaskMessageEntity, TopicTaskMessageDTO): add correlationId property for message tracking

--- backend/super-magic-module/src/Domain/SuperAgent/Entity/TaskMessageEntity.php
+++ backend/super-magic-module/src/Domain/SuperAgent/Entity/TaskMessageEntity.php
@@ -149,6 +149,11 @@ class TaskMessageEntity extends AbstractEntity
      */
     protected ?int $imStatus = null;
 
+    /**
+     * @var null|string 关联ID，用于消息追踪和关联
+     */
+    protected ?string $correlationId = null;
+
     public function __construct(array $data = [])
     {
         $this->id = IdGenerator::getSnowId();
@@ -425,6 +430,17 @@ class TaskMessageEntity extends AbstractEntity
         return $this;
     }
 
+    public function getCorrelationId(): ?string
+    {
+        return $this->correlationId;
+    }
+
+    public function setCorrelationId(?string $correlationId): self
+    {
+        $this->correlationId = $correlationId;
+        return $this;
+    }
+
     public function toArray(): array
     {
         $result = [
@@ -455,6 +471,7 @@ class TaskMessageEntity extends AbstractEntity
             'processed_at' => $this->processedAt,
             'im_seq_id' => $this->imSeqId,
             'im_status' => $this->imStatus,
+            'correlation_id' => $this->correlationId,
         ];
 
         return array_filter($result, function ($value) {
