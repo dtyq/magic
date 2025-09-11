06e84ab00c77bf3809910b2e4d6b1cc2467ebbe6
huzhanhong <huzhanhong@kkgroup.cn>
2025-09-11 18:30:41 +0800

    feat(image-event): ImageGeneratedEntity.php sourceId field allow is null

--- backend/magic-service/app/Application/ModelGateway/Service/LLMAppService.php
+++ backend/magic-service/app/Application/ModelGateway/Service/LLMAppService.php
@@ -1260,7 +1260,7 @@ class LLMAppService extends AbstractLLMAppService
         $imageGeneratedEntity->setTopicId($requestDTO->getTopicId());
         $imageGeneratedEntity->setTaskId($requestDTO->getTaskId());
         $imageGeneratedEntity->setCreatedAt(new DateTime());
-        if ($requestDTO->getTopicId() !== null) {
+        if (! empty($requestDTO->getTopicId())) {
             $imageGeneratedEntity->setSourceType(ImageGenerateSourceEnum::SUPER_MAGIC);
         } else {
             $imageGeneratedEntity->setSourceType(ImageGenerateSourceEnum::API);
