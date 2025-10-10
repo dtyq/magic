29ed26a39d43db1d9e6f4401271b5322447ce7a6
Rock <383329495@qq.com>
2025-10-10 16:19:50 +0800

    refactor(opentracing.php): streamline configuration and enhance tracing options

--- backend/magic-service/config/autoload/opentracing.php
+++ backend/magic-service/config/autoload/opentracing.php
@@ -2,77 +2,40 @@
 
 declare(strict_types=1);
 /**
- * Copyright (c) The Magic , Distributed under the software license
+ * 本文件属于KK馆版权所有，泄漏必究。
+ * This file belong to KKGUAN, all rights reserved.
  */
-use Hyperf\Stringable\Str;
 use Hyperf\Tracer\Adapter\ZipkinTracerFactory;
 use Zipkin\Samplers\BinarySampler;
 
 use function Hyperf\Support\env;
 
-/**
- * 由于阿里云和火山云的URL地址格式不同,因此在此处进行适配解析.
- * @note 注意！opentracing.php文件中，直接配置'endpoint_url' => $endpointUrl ,不要再拼接其他字符
- */
-function multiCloudAdapter(string $endpointUrl): string
-{
-    if (Str::contains($endpointUrl, 'volces')) {
-        // 火山云地址,但是配置文件中的格式可能是按阿里云的表示,无法直接使用
-        preg_match('#(.*)/third_party/zipkin/v2/(\w+)/.*#u', $endpointUrl, $matches);
-        // URL还原
-        $endpointUrl = sprintf('%s/third_party/zipkin/v2/%s/api/v2/spans', $matches[1], $matches[2]);
-    }
-
-    // 阿里云.检测环境变量的值是否包含 /api/v2/spans,如果不包含则添加
-    if (Str::contains($endpointUrl, 'aliyuncs') && ! Str::contains($endpointUrl, 'api/v2/spans')) {
-        $endpointUrl = rtrim($endpointUrl, '/') . '/api/v2/spans';
-    }
-    return $endpointUrl;
-}
-
-$endpointUrl = multiCloudAdapter(env('ZIPKIN_ENDPOINT_URL', ''));
-
 return [
+    'enable_opentracing' => env('ENABLE_OPENTRACING', true),
     'default' => env('TRACER_DRIVER', 'zipkin'),
-    'prefix' => env('TRACER_PREFIX', 'magic-service'),
+    'prefix' => env('TRACER_PREFIX', 'brd'),
     'enable' => [
         'guzzle' => env('TRACER_ENABLE_GUZZLE', true),
         'redis' => env('TRACER_ENABLE_REDIS', true),
         'db' => env('TRACER_ENABLE_DB', true),
-        'simple_db' => env('TRACER_ENABLE_DB', true),
         'elasticsearch' => env('TRACER_ENABLE_ELASTICSEARCH', true),
-        'method' => env('TRACER_ENABLE_METHOD', true),
+        'json_rpc' => env('TRACER_ENABLE_JSONRPC', true),
+        'method' => env('TRACER_ENABLE_METHOD', false),
         'exception' => env('TRACER_ENABLE_EXCEPTION', true),
     ],
     'tracer' => [
         // 主pod
         'zipkin' => [
-            'driver' => ZipkinTracerFactory::class,
+            'driver' => Hyperf\Tracer\Adapter\ZipkinTracerFactory::class,
             'app' => [
-                'name' => sprintf('%s-%s', env('APP_NAME'), env('APP_ENV')),
+                'name' => sprintf('%s-%s', env('APP_NAME'), env('TRACER_PREFIX', env('APP_ENV'))),
                 // Hyperf will detect the system info automatically as the value if ipv4, ipv6, port is null
                 'ipv4' => '0.0.0.0',
                 'ipv6' => null,
                 'port' => 9501,
             ],
             'options' => [
-                'endpoint_url' => $endpointUrl,
-                'timeout' => env('ZIPKIN_TIMEOUT', 1),
-            ],
-            'sampler' => BinarySampler::createAsAlwaysSample(),
-        ],
-        // 多个pod
-        'zipkin-sync' => [
-            'driver' => ZipkinTracerFactory::class,
-            'app' => [
-                'name' => sprintf('%s-%s-sync', env('APP_NAME'), env('APP_ENV')),
-                // Hyperf will detect the system info automatically as the value if ipv4, ipv6, port is null
-                'ipv4' => '0.0.0.0',
-                'ipv6' => null,
-                'port' => 9501,
-            ],
-            'options' => [
-                'endpoint_url' => $endpointUrl,
+                'endpoint_url' => env('ZIPKIN_ENDPOINT_URL', '') . '/api/v2/spans',
                 'timeout' => env('ZIPKIN_TIMEOUT', 1),
             ],
             'sampler' => BinarySampler::createAsAlwaysSample(),
@@ -109,6 +72,7 @@ return [
             'path' => 'request.path',
             'method' => 'request.method',
             'header' => 'request.header',
+            'uri' => 'request.uri',
         ],
         'coroutine' => [
             'id' => 'coroutine.id',
