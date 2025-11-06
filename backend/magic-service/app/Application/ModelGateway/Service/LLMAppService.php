61cbce27a77cbc651f6a9688ad19074942e203d3
lihq1403 <lihqing1403@gmail.com>
2025-11-06 17:37:25 +0800

    refactor(LLMAppService): remove unused Carbon import

--- backend/magic-service/app/Application/ModelGateway/Service/LLMAppService.php
+++ backend/magic-service/app/Application/ModelGateway/Service/LLMAppService.php
@@ -56,7 +56,6 @@ use App\Infrastructure\Util\SSRF\SSRFUtil;
 use App\Infrastructure\Util\StringMaskUtil;
 use App\Interfaces\Authorization\Web\MagicUserAuthorization;
 use App\Interfaces\ModelGateway\Assembler\EndpointAssembler;
-use Carbon\Carbon;
 use DateTime;
 use Dtyq\AsyncEvent\AsyncEventUtil;
 use Dtyq\CloudFile\Kernel\Struct\UploadFile;
