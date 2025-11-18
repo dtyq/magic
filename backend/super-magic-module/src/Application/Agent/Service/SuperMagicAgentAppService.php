b0c83444d26276871bebb438b0e67256da08b5be
shensiming <shensiming@kkgroup.cn>
2025-11-18 16:49:56 +0800

    fix super-magic/agents/queries not icon_type

--- backend/super-magic-module/src/Application/Agent/Service/SuperMagicAgentAppService.php
+++ backend/super-magic-module/src/Application/Agent/Service/SuperMagicAgentAppService.php
@@ -70,7 +70,7 @@ class SuperMagicAgentAppService extends AbstractSuperMagicAppService
         // 目前只能查询自己的，全量查询
         $query->setCreatorId($authorization->getId());
         $page->disable();
-        $query->setSelect(['id', 'code', 'name', 'description', 'icon']); // Only select necessary fields for list
+        $query->setSelect(['id', 'code', 'name', 'description', 'icon', 'icon_type']); // Only select necessary fields for list
 
         $result = $this->superMagicAgentDomainService->queries($dataIsolation, $query, $page);
 
